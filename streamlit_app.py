9d8f7f4cb6cf28a65265ddaa8ed0deb90a624cde
Jenn <96705173+jennerous@users.noreply.github.com>
2023-05-16 09:56:10 -0700

    Update streamlit_app.py
    added fruityvice response json
    added fruityvice dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,3 +27,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)
+
+# Reads from fruityvice response json
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# creates dataframe
+streamlit.dataframe(fruityvice_normalized)
+
